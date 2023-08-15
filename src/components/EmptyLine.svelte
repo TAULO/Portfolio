41035c670abeb514e79de4330ebc65dd23a79a2c
THOMAS TAULO <thomas@sirenia.eu>
2023-08-15 22:31:23 +0200

    move to own component

---
+++ src/components/EmptyLine.svelte
@@ -0,0 +1,10 @@
+<!-- set color same as background to create emtpy line. Bad but temporary solution -->
+<div class="d-flex hide" style="color: #1f1f1f;">
+    placeholder
+</div>
+
+<style>
+    .hide {
+        visibility: hidden;
+    }
+</style>
