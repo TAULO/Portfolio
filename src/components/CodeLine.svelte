48f5e46c7c4412477e2bf38485608902963f3acb
THOMAS TAULO <thomas@sirenia.eu>
2023-08-15 22:30:18 +0200

    move to own compoment

---
+++ src/components/CodeLine.svelte
@@ -0,0 +1,14 @@
+<div class="d-inline-flex flex-column align-items-end" id="code-line-container">
+    {#each Array(100).fill() as _, index}
+        <div>
+            {index + 1} 
+        </div>
+    {/each}
+</div>  
+
+
+<style>
+    #code-line-container {
+        color: #68707e;
+    }
+</style>
