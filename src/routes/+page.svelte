431068c95433b9a0a2aebbab594445c53e6eb58d
THOMAS TAULO <thomas@sirenia.eu>
2023-08-15 22:33:54 +0200

    lots of changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,85 +1,106 @@
 <script>
+    import CodeLine from "../components/CodeLine.svelte";
     import CodeTag from "../components/CodeTag.svelte";
     import About from "../components/About.svelte";
+    import EmptyLine from "../components/EmptyLine.svelte";
+    import Comment from "../components/Comment.svelte";
+    import OnClick from "../components/OnClick.svelte";
+    import Stack from "../components/Stack.svelte";
+    import Icon from "../components/Service.svelte";
 </script>
 
-<div class="container-fluid">
-    <p class="">
+<div class="container-fluid">   
+    <header class="route py-3">
         src > routes > index.js
-    </p>
-    
-    <p class="">
+    </header>	
+    <div class="mb-2" style="color: #9e9e9e;">
         Thomas Taulo, 1 second ago | 1 author (Thomas Taulo)
-    </p>
-    <div class="d-flex">
-        
-        <div class="d-inline-flex flex-column align-items-end ms-3" id="code-line-container">
-            {#each Array(100).fill() as _, index}
-                <div>
-                    {index + 1} 
-                </div>
-            {/each}
-        </div>    
-        <div class="d-flex flex-column ms-4">
-            <div id="comment">
-                // about me
-            </div>
+    </div>     
+    <div class="d-flex">  
+
+        <CodeLine />
+
+        <div class="d-flex flex-column ms-4 color_transition">
+            <Comment comment="about me"/>
             <CodeTag tagName="script" --tag-color=#5296cb>
                 <slot>
                     <About/>
                 </slot>
             </CodeTag>
 
-            <div class="empty_line">
-                placeholder
-            </div>
+            <EmptyLine/>
 
-            <div id="comment">
-                // school projects
-            </div>
-            <CodeTag tagName="Project" --tag-color=#49c0aa>
-            </CodeTag>
-            
-            <div class="empty_line">
-                placeholder
-            </div>
-
-            <div id="comment">
-                // work projects
-            </div>
-            <CodeTag tagName="Project" --tag-color=#49c0aa>
+            <Comment comment="school projects"/>
+            <CodeTag tagName="Project" projectNameProp="Vending Macine" --tag-color=#49c0aa>
+                <slot>
+                    <Comment comment="click to view this project"/>
+                    <OnClick/>
+                </slot>
             </CodeTag>
 
-            <div class="empty_line">
-                placeholder
-            </div>
+            <EmptyLine/>
             
-            <div id="comment">
-                // hobby / personal projects
-            </div>
+            <CodeTag tagName="Project" projectNameProp="Task Mining" --tag-color=#49c0aa>
+                <slot>
+                    <Comment comment="my final exam project for the computer science education program"/>
+                    <Comment comment="the project involved developing a software-related product for the company where I completed a 6-month internship"/>
+                    <EmptyLine/>
+                    <Comment comment="click to view this project"/>
+                    <OnClick/>
+                    <EmptyLine/>
+                    <Comment comment="the tech stack"/>
+                    <CodeTag tagName="Stack">
+                        <slot>
+                            <Icon name="React"/>
+                            <Icon name="ASP.NET RESTful API"/>
+                            <i class="bi bi-0-circle" style="color: green;"></i>
+                        </slot>
+                    </CodeTag>
+                </slot>
+            </CodeTag>
             
-            <CodeTag tagName="Project" --tag-color=#49c0aa>
+            <EmptyLine/>
 
+            <Comment comment="work projects"/>
+            <CodeTag tagName="Project" --tag-color=#49c0aa>
+                <slot>
+                    <Comment comment="TODO"/>
+                </slot>
+            </CodeTag>
+
+            <EmptyLine/>
+            
+            <Comment comment="hobby / personal projects"/>
+            <CodeTag tagName="Project" projectNameProp="ringe-og-omegns-biavlerforening.dk" --tag-color=#49c0aa>
+                <slot>
+                    <OnClick/>
+                </slot>
+            </CodeTag>
+
+            <EmptyLine/>
+
+            <CodeTag tagName="Project" projectNameProp="ATaulo.dk" --tag-color=#49c0aa>
+                <slot>
+                    <OnClick/>
+                </slot>
             </CodeTag>
         </div>
     </div>
 </div>
 
 <style>
-    #code-line-container {
-        color: #68707e;
-        height: 100%;
-    }
-    p {
-        color: #68707e;
-    }
-    #comment {
-        color: #679451;
-    }
+    .color_transition {
+        color: blue;
+    /* Add any styles you want for the container here */
+  }
 
-    /* set color same as background to create emtpy line. Bad but temporary solution */
-    .empty_line {
-        display: flex;
-        color: #1f1f1f;;
-    }
+  .color_transition * {
+    transition: color 0.6s ease, background-color 0.3s ease; /* Add transition for color and background-color */
+  }
+
+  .color_transition:hover * {
+    color: green; /* New text color on hover */
+    background-color: yellow; /* New background color on hover */
+    /* Add any other style changes for hover state */
+  }
 </style>
